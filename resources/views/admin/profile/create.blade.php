c4aa507fe6ee0d574758fd3cbfbb6be2486c92e3
Shinichi Wakui <shinichiwakui2020@gmail.com>
2020-08-30 03:44:56 +0000

    課題:PHP/Laravel 12 ユーザー認証を実装する

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,10 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
-
-
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'プロフィールの新規作成画面')
-
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール作成画面test</h2>
+                <h2>You can make your profile !!</h2>
             </div>
         </div>
     </div>
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -24,7 +24,7 @@
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         {{-- この章の後半で作成するCSSを読み込みます --}}
-        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
@@ -41,12 +41,8 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                             <!-- Authentication Links -->
+                             {{-- 以下を追記 --}}
+                        <!-- Authentication Links -->
                         {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
                         @guest
                             <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
@@ -70,6 +66,11 @@
                                 </div>
                             </li>
                             @endguest
+                            {{-- 以上までを追記 --}}
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
                         </ul>
                     </div>
                 </div>
