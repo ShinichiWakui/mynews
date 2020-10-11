21886579cb1b7e45fe2109bf1798e364e4b1928e
Shinichi Wakui <shinichiwakui2020@gmail.com>
2020-10-11 13:12:52 +0000

    PHP/Laravel12 課題

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,20 @@ Route::get('/', function () {
 
 });
 
+/* 修正前
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit',  'Admin\ProfileController@edit')->middleware('auth');
-    
 });
+*/
 
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit',  'Admin\ProfileController@edit');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
